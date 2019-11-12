454249d6f8a2f1fbe688f498717929c6d2850de0
anairaquezada <quezadaa@nmsu.edu>
2019-11-11 19:26:32 -0700

    Update phase2.1.py
    Edited the formating on the query function, it should print uniformly now in the window

--- phase2.1.py
+++ phase2.1.py
@@ -36,30 +36,49 @@ def getQuery():
     else:
             
         query = "SELECT * FROM " + tableName + ";"
-        con = MySQL.connect('localhost', 'root', 'databaseTeam#6', 'OffensiveNFLPlayers')
+        con = MySQL.connect('localhost', 'root', 'Databases19', 'ProjectDB')
 
         with con:
             cur = con.cursor()
             cur.execute(query)
             rows = cur.fetchall()
-                
+            desc = cur.description      #this gets the attribute names
             if (tableName.lower() == "players"):
-                output = "\n   Player ID | First Name | Last Name | Team ID | Position | Touchdowns | Total Yards | Salary"
-                output = output + "\n   ----------+------------+-----------+---------+----------+------------+-------------+---------\n"
-                for row in rows:
-                    output = output + "    " + str(row[0]) + "\t " + row[1] + " \t   " + row[2] + "\t   " + str(row[3]) + " \t    " + row[4] + "   \t  " + str(row[5]) + "   \t   " + str(row[6]) + "   \t " + str(row[7]) + "\n"
-                print(output)
+                #output = "\n   Player ID | First Name | Last Name | Team ID | Position | Touchdowns | Total Yards | Salary"
+                #output = output + "\n   ----------+------------+-----------+---------+----------+------------+-------------+---------\n"
+                #for row in rows:
+                #    output = output + "    " + str(row[0]) + "\t " + row[1] + " \t   " + row[2] + "\t   " + str(row[3]) + " \t    " + row[4] + "   \t  " + str(row[5]) + "   \t   " + str(row[6]) + "   \t " + str(row[7]) + "\n"
+                #print(output)
+                      #prints column names
+                
+                #print (rows)
+                output = ("{0:>0} {1:>10} {2:>12} {3:>8} {4:>12} {5:>12} {6:>12} {7:>10}".format(desc[0][0], desc[1][0], desc[2][0], desc[3][0], desc[4][0], desc[5][0], desc[6][0], desc[7][0])) + "\n" #8 columns
+                for row in rows:    
+                    output = output + ("{0:>0} {1:>10} {2:>15} {3:>8} {4:>12} {5:>12} {6:>12} {7:>10}".format(row[0], row[1], row[2], row [3], row[4], row[5], row[6], row[7])) + "\n"
+       
                 
             elif (tableName.lower() == "games"):
-                print ("\n   Game ID\t|\tDate\t|\tStadium\t|\tResult\t|\tAttendance\t|\tTicket revenue")
+                #print ("\n   Game ID\t|\tDate\t|\tStadium\t|\tResult\t|\tAttendance\t|\tTicket revenue")
+                output = ("{0:>0} {1:>10} {2:>35} {3:>10} {4:>15} {5:>15}".format(desc[0][0], desc[1][0], desc[2][0], desc[3][0], desc[4][0], desc[5][0])) + "\n" #6 attributes
+                for row in rows:    
+                    output = output + ("{0:>0} {1:} {2:>35} {3:>10} {4:>15} {5:>15}".format(row[0], row[1], row[2], row [3], row[4], row[5])) + "\n"
+       
                 
             elif (tableName.lower() == "teams"):
-                print ("\n   Team ID\t|\tTeam Name\t|\tCity")
-                
+               #print ("\n   Team ID\t|\tTeam Name\t|\tCity")
+               output = ("{0:>0} {1:>12} {2:>15}".format(desc[0][0], desc[1][0], desc[2][0])) + "\n" #8 columns
+               for row in rows:    
+                    output = output + ("{0:>0} {1:>15} {2:>15} ".format(row[0], row[1], row[2])) + "\n"
+       
+               
             elif (tableName.lower() == "play"):
-                print ("\n   PlayerID\t|\tGame ID")
+                #print ("\n   PlayerID\t|\tGame ID")
+                output = ("{0:>0} {1:>7}".format(desc[0][0], desc[1][0])) + "\n" #8 columns
+                for row in rows:    
+                    output = output + ("{0:>0} {1:>10}".format(row[0], row[1])) + "\n"
+       
             
-        queryTextbox.insert(END, output)
+    queryTextbox.insert(END, output)
  
  
  
